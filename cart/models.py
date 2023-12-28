c7688f6fe00582b4f0b46c02ecf4f58eaa3f34e2
unknown <mtharwat@onesolutionc.com>
2023-12-28 23:19:22 +0200

    update relation with cart to one to one with customer

--- cart/models.py
+++ cart/models.py
@@ -6,7 +6,7 @@ class Cart(models.Model):
     """
     Cart Model.
     """
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class CartItem(models.Model):
