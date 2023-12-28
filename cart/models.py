0e3e12a0496a4c74c2a30b8e786528fc601d84c8
unknown <mtharwat@onesolutionc.com>
2023-12-28 13:22:49 +0200

    add Cart and CartItem model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from customer.models import Customer
+from product.models import Product
 
-# Create your models here.
+class Cart(models.Model):
+    """
+    Cart Model.
+    """
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    """
+    CartItem Model.
+    """
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
