6be71da17a83128d26d48ccd0e9faafbf89b6103
unknown <mtharwat@onesolutionc.com>
2023-12-28 15:53:21 +0200

    add update_cart_item_quantity to urls

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import CartListView, CartCreateView, add_to_cart, remove_from_cart
+from .views import (
+    CartListView,
+    CartCreateView,
+    add_to_cart,
+    remove_from_cart,
+    update_cart_item_quantity
+)
 urlpatterns = [
-    path('cart-list/',CartListView.as_view(), name='cart_list'),
-    path('cart-create/',CartCreateView.as_view(), name='cart_create'),
-    path('add-item/',add_to_cart, name='add_item_to_cart'),
-    path('remove-item/',remove_from_cart, name='remove_item_from_cart'),
+    path('cart-list/', CartListView.as_view(), name='cart_list'),
+    path('cart-create/', CartCreateView.as_view(), name='cart_create'),
+    path('add-item/', add_to_cart, name='add_item_to_cart'),
+    path('remove-item/', remove_from_cart, name='remove_item_from_cart'),
+    path('update-item-quantity/', update_cart_item_quantity, name='remove_item_from_cart')
 ]
