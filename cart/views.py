48ee3eb8d922450ac57d5bab42b8b2cc74d9b1fa
unknown <mtharwat@onesolutionc.com>
2023-12-28 13:34:59 +0200

    add create and list views for cart

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Cart
+from .serializers import CartListSerializer, CartCreateSerializer
 
-# Create your views here.
+
+class CartListCreateView(generics.ListCreateAPIView):
+    """
+    View to list all serilized carts in form of CartListSerializer.
+    """
+    queryset = Cart.objects.all()
+    serializer_class = CartListSerializer
+
+class CartListCreateView(generics.ListCreateAPIView):
+    """
+    View to create Cart for customer in form of CartCreateSerializer.
+    """
+    queryset = Cart.objects.all()
+    serializer_class = CartCreateSerializer
