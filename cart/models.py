42ed26fcf679e5c59edc553920f064467ad7c7dd
unknown <mtharwat@onesolutionc.com>
2023-12-28 21:47:55 +0200

    add migrations fro cartitem boolean field ordered

--- cart/models.py
+++ cart/models.py
@@ -16,3 +16,4 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+    ordered = models.BooleanField(default=False)
