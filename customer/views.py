2ffce17f3d3e8a4ea5f2f3c4632836511974614d
unknown <mtharwat@onesolutionc.com>
2023-12-28 10:32:31 +0200

    add views to customer

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Customer
+from .serializers import CustomerListSerializer, CustomerCreateSerializer
 
-# Create your views here.
+class CustomerListCreateView(generics.ListAPIView):
+    """
+    View to list all serilized customer in form of CustomerListSerializer.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerListSerializer
+
+
+class CustomerListCreateView(generics.CreateAPIView):
+    """
+    View to create customer in form of CustomerCreateSerializer.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerCreateSerializer
