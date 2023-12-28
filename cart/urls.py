8de753ff6a43f2c4c5a2786b22142dbcc7d76807
unknown <mtharwat@onesolutionc.com>
2023-12-28 15:01:00 +0200

    add remove_from_cart view to urls

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CartListView, CartCreateView, add_to_cart
+from .views import CartListView, CartCreateView, add_to_cart, remove_from_cart
 urlpatterns = [
     path('cart-list/',CartListView.as_view(), name='cart_list'),
     path('cart-create/',CartCreateView.as_view(), name='cart_create'),
     path('add-item/',add_to_cart, name='add_item_to_cart'),
+    path('remove-item/',remove_from_cart, name='remove_item_from_cart'),
 ]
