3ea2441d8b22b75c6cd7135ae3cc105afcac090d
unknown <mtharwat@onesolutionc.com>
2023-12-28 13:37:55 +0200

    update and comment views for create and list products

--- product/views.py
+++ product/views.py
@@ -4,9 +4,15 @@ from .serializers import ProductListSerializer, ProductCreateSerializer
 
 
 class ProductListView(generics.ListCreateAPIView):
+    """
+    View to list all serilized products in form of ProductListSerializer.
+    """
     queryset = Product.objects.all()
     serializer_class = ProductListSerializer
 
 class ProductCreateView(generics.ListCreateAPIView):
+    """
+    View to create Product in form of ProductCreateSerializer.
+    """
     queryset = Product.objects.all()
     serializer_class = ProductCreateSerializer
