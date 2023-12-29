ce510ca7e846f4770bfda6365cbfb1c7294632c3
unknown <mtharwat@onesolutionc.com>
2023-12-29 21:45:50 +0200

    update urls for list and create

--- cart/urls.py
+++ cart/urls.py
@@ -9,8 +9,8 @@ from .views import (
     cart_details
 )
 urlpatterns = [
-    path('cart-list/', CartListView.as_view(), name='cart_list'),
-    path('cart-create/', CartCreateView.as_view(), name='cart_create'),
+    path('list/', CartListView.as_view(), name='cart_list'),
+    path('create/', CartCreateView.as_view(), name='cart_create'),
     path('add-item/', add_to_cart, name='add_item_to_cart'),
     path('remove-item/', remove_from_cart, name='remove_item_from_cart'),
     path('update-item-quantity/', update_cart_item_quantity, name='update_cart_item_quantity'),
