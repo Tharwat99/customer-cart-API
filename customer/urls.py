5803f2d95beaf058cb892cbc45ee5180f5449839
unknown <mtharwat@onesolutionc.com>
2023-12-30 00:20:51 +0200

    update urls for customer

--- customer/urls.py
+++ customer/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import CustomerListView, CustomerCreateView
 urlpatterns = [
-    path('customer-list/',CustomerListView.as_view(), name='customer_list'),
-    path('customer-create/',CustomerCreateView.as_view(), name='customer_create'),
+    path('list/',CustomerListView.as_view(), name='customer_list'),
+    path('create/',CustomerCreateView.as_view(), name='customer_create'),
 ]
