023b5a0749f83612b6c75555126041b85460d2e8
unknown <mtharwat@onesolutionc.com>
2023-12-29 17:09:01 +0200

    update update_cart_item_quantity

--- cart/views.py
+++ cart/views.py
@@ -87,7 +87,7 @@ def update_cart_item_quantity(request):
     if new_quantity < 0:
         return Response({'error': 'Quantity must be more than zero'}, status=status.HTTP_400_BAD_REQUEST)
     try:
-        cart_item = CartItem.objects.get(id=cart_item_id)
+        cart_item = CartItem.objects.get(id=cart_item_id, ordered=False)
     except CartItem.DoesNotExist:
         return Response({"error": "Invalid cart item."}, status=status.HTTP_404_NOT_FOUND)
     
