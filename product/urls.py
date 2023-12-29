4a03fa5e953431fbb170635b19a4a0e0a481642c
unknown <mtharwat@onesolutionc.com>
2023-12-30 00:23:41 +0200

    update urls and views for product

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ProductListView, ProductCreateView
 urlpatterns = [
-    path('product-list/',ProductListView.as_view(), name='product_list'),
-    path('product-create/',ProductCreateView.as_view(), name='product_create'),
+    path('list/',ProductListView.as_view(), name='product_list'),
+    path('create/',ProductCreateView.as_view(), name='product_create'),
 ]
--- product/views.py
+++ product/views.py
@@ -3,14 +3,14 @@ from .models import Product
 from .serializers import ProductListSerializer, ProductCreateSerializer
 
 
-class ProductListView(generics.ListCreateAPIView):
+class ProductListView(generics.ListAPIView):
     """
     View to list all serilized products in form of ProductListSerializer.
     """
     queryset = Product.objects.all()
     serializer_class = ProductListSerializer
 
-class ProductCreateView(generics.ListCreateAPIView):
+class ProductCreateView(generics.CreateAPIView):
     """
     View to create Product in form of ProductCreateSerializer.
     """
