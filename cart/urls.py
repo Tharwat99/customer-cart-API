c9d14965a5789448334d80285a94292f93647e77
unknown <mtharwat@onesolutionc.com>
2023-12-29 21:46:49 +0200

    update urls for cart

--- cart/urls.py
+++ cart/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('add-item/', add_to_cart, name='add_item_to_cart'),
     path('remove-item/', remove_from_cart, name='remove_item_from_cart'),
     path('update-item-quantity/', update_cart_item_quantity, name='update_cart_item_quantity'),
-    path('cart-checkout/', cart_checkout, name='cart_checkout'),
-    path('cart-details/', cart_details, name='cart_details')
+    path('checkout/', cart_checkout, name='cart_checkout'),
+    path('details/', cart_details, name='cart_details')
 ]
