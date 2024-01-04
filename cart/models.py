9b1b65ec9d5717e301f733bcc76e63b5cc928900
unknown <mtharwat@onesolutionc.com>
2024-01-05 00:11:23 +0200

    update models add verbose_name for fields

--- cart/models.py
+++ cart/models.py
@@ -6,7 +6,7 @@ class Cart(models.Model):
     """
     Cart Model related to customer one to one rel.
     """
-    customer = models.OneToOneField('customer.Customer', on_delete=models.CASCADE)
+    customer = models.OneToOneField('customer.Customer', on_delete=models.CASCADE, verbose_name = _('customer'))
 
     def __str__(self):
         return self.customer.name
@@ -25,12 +25,12 @@ class CartItem(models.Model):
         (REMOVED, _('Item Removed')),
         (CHECKOUT, _('Item Checkout')),
     )
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
-    status = models.PositiveSmallIntegerField(null = True, blank = True, choices=ITEM_STATUS, default = ADDED)
-    created_at = models.DateTimeField(auto_now_add=True, db_index = True)
-    updated_at = models.DateTimeField(auto_now=True, db_index = True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, verbose_name = _('cart'))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name = _('product'))
+    quantity = models.PositiveIntegerField(verbose_name = _('quantity'))
+    status = models.PositiveSmallIntegerField(null = True, blank = True, choices=ITEM_STATUS, default = ADDED, verbose_name = _('status'))
+    created_at = models.DateTimeField(auto_now_add=True, db_index = True, verbose_name = _('created_at'))
+    updated_at = models.DateTimeField(auto_now=True, db_index = True, verbose_name = _('updated_at'))
     
     class Meta:
         ordering = ['-updated_at']
