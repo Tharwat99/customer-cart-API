8945535c8bfaebb1d0ef19a7938d8ab4e2abfd59
unknown <mtharwat@onesolutionc.com>
2024-01-05 00:16:45 +0200

    update models add verbose_name for fields

--- cart/models.py
+++ cart/models.py
@@ -6,8 +6,13 @@ class Cart(models.Model):
     """
     Cart Model related to customer one to one rel.
     """
-    customer = models.OneToOneField('customer.Customer', on_delete=models.CASCADE, verbose_name = _('customer'))
-
+    customer = models.OneToOneField('customer.Customer', on_delete=models.CASCADE, verbose_name = _('Customer'))
+    
+    class Meta:
+        ordering = ['-id']
+        verbose_name = _('Cart')
+        verbose_name_plural = _('Carts')
+        
     def __str__(self):
         return self.customer.name
 
@@ -25,16 +30,18 @@ class CartItem(models.Model):
         (REMOVED, _('Item Removed')),
         (CHECKOUT, _('Item Checkout')),
     )
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, verbose_name = _('cart'))
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name = _('product'))
-    quantity = models.PositiveIntegerField(verbose_name = _('quantity'))
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, verbose_name = _('Cart'))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name = _('Product'))
+    quantity = models.PositiveIntegerField(verbose_name = _('Quantity'))
     status = models.PositiveSmallIntegerField(null = True, blank = True, choices=ITEM_STATUS, default = ADDED, verbose_name = _('status'))
-    created_at = models.DateTimeField(auto_now_add=True, db_index = True, verbose_name = _('created_at'))
-    updated_at = models.DateTimeField(auto_now=True, db_index = True, verbose_name = _('updated_at'))
+    created_at = models.DateTimeField(auto_now_add=True, db_index = True, verbose_name = _('Created at'))
+    updated_at = models.DateTimeField(auto_now=True, db_index = True, verbose_name = _('Updated at'))
     
     class Meta:
         ordering = ['-updated_at']
-        
+        verbose_name = _('Cart Item')
+        verbose_name_plural = _('Cart Items')
+
     def __str__(self):
         return self.product.name
     
